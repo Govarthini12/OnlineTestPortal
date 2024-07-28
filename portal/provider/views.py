25d320c70ee9c9f816baa10a61520975c1adca0e
joabeliot <joabeliot@gmail.com>
2024-07-28 17:23:02 +0530

    Portal Template

--- portal/provider/views.py
+++ portal/provider/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        return context
+    
+class ResultView(TemplateView):
+    template_name = 'result.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        return context
+    
+def calculations(request):
+    result=0
+    return result
+
+def download(request):
+    path=""
+    return path
